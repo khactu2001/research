eef17d7eb201320681aa271ad208d1f73d17051a
LeeMac <tulk@brickmate.kr>
2023-08-27 20:20:54 +0700

    first commit

---
+++ android/app/src/main/java/com/research/MainActivity.java
@@ -0,0 +1,18 @@
+package com.research;
+
+import com.reactnativenavigation.NavigationActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.os.Bundle;
+
+public class MainActivity extends NavigationActivity {
+
+  
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+  
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,38 @@
+// In index.js of a new project
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { Navigation } from 'react-native-navigation';
+
+const HomeScreen = (props: any) => {
+  return (
+    <View style={styles.root}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+Navigation.registerComponent('Home', () => HomeScreen);
+
+Navigation.events().registerAppLaunchedListener(async () => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'Home'
+            }
+          }
+        ]
+      }
+    }
+  });
+});
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'whitesmoke'
+  }
+});
